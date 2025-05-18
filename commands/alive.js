bb9f1eb541f16c4b2dfbeaf7da888e668d20013d
Cnamy-rpg <ho.usseindam.erji2@gmail.com>
2025-05-18 13:10:15 +0200

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,7 @@
 const settings = require("../settings");
 async function aliveCommand(sock, chatId) {
     try {
-        const message = `*🤖 Knight Bot is Active!*\n\n` +
+        const message = `*😘 CNAMY-BOT-V1 is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
@@ -19,7 +19,7 @@ async function aliveCommand(sock, chatId) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'CNAMY-BOT-V1',
                     serverMessageId: -1
                 }
             }
