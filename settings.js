8f35d8eb371f95994711f907b007156fab4660ed
Cnamy-rpg <ho.usseindam.erji2@gmail.com>
2025-05-18 10:23:14 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Cnamy-Bot-V1',
+  author: 'Cnamy‎',
+  botName: "Cnamy-Bot-V1",
+  botOwner: 'Cnamy', // Your name
+  ownerNumber: '491608888888', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
