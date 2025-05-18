e9350dde6efb8a8af0e1da3a2563000aaa56aa7f
Cnamy-rpg <ho.usseindam.erji2@gmail.com>
2025-05-18 13:38:51 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,11 +1,11 @@
 async function githubCommand(sock, chatId) {
-    const repoInfo = `*🤖 KnightBot MD*
+    const repoInfo = `*😘 CNAMY-BOT-V1*
 
 *📂 GitHub Repository:*
-https://github.com/mruniquehacker/Knightbot-MD
+https://github.com/Cnamy-rpg/CNAMY-BOT-V1
 
 *📢 Official Channel:*
-https://youtube.com/@mr_unique_hacker
+https://whatsapp.com/channel/0029Vb6AYn8CcW4pDk4lcI2e
 
 _Star ⭐ the repository if you like the bot!_`;
 
@@ -16,8 +16,8 @@ _Star ⭐ the repository if you like the bot!_`;
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029Vb6AYn8CcW4pDk4lcI2e@newsletter',
+                    newsletterName: 'CNAMY-BOT-V1',
                     serverMessageId: -1
                 }
             }
