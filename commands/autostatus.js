278170fa9a6defb5cd7d5fe7209d2bd24b9276c3
Cnamy-rpg <ho.usseindam.erji2@gmail.com>
2025-05-18 13:18:49 +0200

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '0029Vb6AYn8CcW4pDk4lcI2e@newsletter',
+            newsletterName: 'CNAMY-BOT-V1',
             serverMessageId: -1
         }
     }
