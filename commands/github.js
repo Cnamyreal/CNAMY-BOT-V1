b4ecc80a6ce41d9b0d73e526037aeacde9934915
Cnamyreal <housseinda.merji2@gmail.com>
2025-05-18 19:05:48 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,11 +1,13 @@
 async function githubCommand(sock, chatId) {
-    const repoInfo = `*🤖 KnightBot MD*
+    const repoInfo = `*😘 CNAMY-BOT-V1*
 
 *📂 GitHub Repository:*
-https://github.com/mruniquehacker/Knightbot-MD
+https://github.com/Cnamyreal/CNAMY-BOT-V1.git
 
 *📢 Official Channel:*
-https://youtube.com/@mr_unique_hacker
+https://whatsapp.com/channel/0029Vb6AYn8CcW4pDk4lcI2e
+Subscribe Ryu028 Music
+https://youtube.com/@ryu028-b5t?si=U3BLz2UjbDRLS4yH
 
 _Star ⭐ the repository if you like the bot!_`;
 
@@ -17,7 +19,7 @@ _Star ⭐ the repository if you like the bot!_`;
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'CNAMY-BOT-V1',
                     serverMessageId: -1
                 }
             }
